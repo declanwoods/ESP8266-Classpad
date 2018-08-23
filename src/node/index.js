52e0331a104113794d98a50dac479abc03ec9f80
Declan Woods <docdecdoc@hotmail.com>
2018-08-23 23:34:50 +0800

    Created base layout without logic, default constructor

--- src/node/index.js
+++ src/node/index.js
@@ -1,19 +1,44 @@
-class ClasspadProto {
-	constructor() {
-
+class Classpad {
+	constructor(port) {
+		self.port = port
+		self.baudrate = 38400
+		self.bytesize = 7
+		self.serial = null
+		self.noSerial = port==null
+		self.header = "3A4E446400010001"
 	}
-	open() {
-
+	open(s=null) {
+		if (s != null) {
+			s = null;
+		}
+		this.serial = s;
+		return true;
 	}
 	close() {
-		
+		if (this.serial) {
+			this.serial.close();
+		}
+		return true;
 	}
-	writeString() {
-
+	writeString(text) {
+		if !(this.serial==null) && !(self.noSerial==null) {
+			return false;
+		}
 	}
-	writeInteger() {
+	writeInteger(num) {
+		if !(this.serial==null) && !(self.noSerial==null) {
+			return false;
+		}
+	}
+	writeFloat(num) {
+		if !(this.serial==null) && !(self.noSerial==null) {
+			return false;
+		}
 
+		if (Math.floor(num) == num) {
+			return writeInteger(Math.floor(num));
+		}
 	}
 }
 
-module.exports = ClasspadProto;
+module.exports = Classpad;
